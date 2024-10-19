289a1846eae709409c5a4115f56f61817b38454a
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-10-19 01:30:47 -0700

    add stuff

--- HalloweenJam/Assets/Scripts/Utility/SceneController.cs
+++ HalloweenJam/Assets/Scripts/Utility/SceneController.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static SceneController Instance { get; private set;}
+    private void Awake() {
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LoadScene(int buildIndex) {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void Quit() {
+        Application.Quit();
     }
 }
